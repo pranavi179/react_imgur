fb238315e306519aaaff6ede7f630e9d67ffe9ea
pranavi179 <pranavivelisoju@gmail.com>
2019-10-31 01:37:35 +0530

    Added App, Form and Table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Table from './Table.js';
+import Form from './form.js';
 
 // function App() {
 //   return (
@@ -24,17 +25,52 @@ import Table from './Table.js';
 //   );
 // }
 
+// bind the context
+// class MyContainer extends Component {
+//
+//     constructor(props) {
+//         super(props);
+//         this.testVarible= "this is a test";
+//     }
+//
+//     onMove = () => {
+//         console.log(this.testVarible);
+//     }
+// }
+
 class App extends Component {
-  render() {
-    return (
-      // <div className = "App">
-      // <h1>pranavi</h1>
-      <div className="container">
-        <Table />
-</div>
-      // </div>
-    )
+
+  state = {
+     characters: [],
   }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+    this.setState({
+      characters: characters.filter((character, i ) => {
+        return i!== index
+      }),
+    })
+  }
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character]})
+  }
+  render() {
+    const { characters } = this.state
+      return (
+        // <div className = "App">
+        // <h1>pranavi</h1>
+        <div className="container">
+
+          < Table characterData= { characters } removeCharacter = { this.removeCharacter} />
+          <Form handleSubmit={this.handleSubmit} />
+          </div>
+
+      );
+    }
 }
+
+
+
 
 export default App;
